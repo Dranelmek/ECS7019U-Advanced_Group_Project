8d6a2a9257095c5013478b2bfa176995a8cffc17
Lenard <bebobhd@gmail.com>
2024-01-22 02:46:01 +0000

    added react router

--- Frontend/councilSite/frontend/src/components/App.js
+++ Frontend/councilSite/frontend/src/components/App.js
@@ -1,16 +1,19 @@
 import './styles/App.css';
 import Navbar from './Navbar';
 import Login from './Login';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      
-        
-        
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="login" element={<Login />} />
+            
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

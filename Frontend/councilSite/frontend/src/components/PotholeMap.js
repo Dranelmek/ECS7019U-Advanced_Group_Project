323987510fc7f662bd409ffd69843264de4f6f3c
Lenard <bebobhd@gmail.com>
2024-02-26 00:03:09 +0000

    added map functionality

--- Frontend/councilSite/frontend/src/components/PotholeMap.js
+++ Frontend/councilSite/frontend/src/components/PotholeMap.js
@@ -1,18 +1,27 @@
 import './styles/PotholeMap.css';
-import {
-    APIProvider,
-    Map,
-    AdvancedMarker,
-    Pin,
-    InfoWindow,
-} from "@vis.gl/react-google-maps"
+import GoogleMapReact from 'google-map-react'
 
 function PotholeMap() {
+
+    const defaultProps = {
+        center: {
+          lat: 51.5205,
+          lng: -0.0375
+        },
+        zoom: 15
+    };
+
     return (
         <header className="Map-header">
             <div className="Map-container">
                 <div className="Map-wrapper">
+                    <GoogleMapReact
+                        bootstrapURLKeys={{ key: "AIzaSyAi-NmWDRv39zsehqwfUC1k-8Uagat5Jyk" }}
+                        defaultCenter={defaultProps.center}
+                        defaultZoom={defaultProps.zoom}
+                    >
 
+                    </GoogleMapReact>
                 </div>
             </div>
         </header>
